ece6f92ce5ddb5658fc017df7847981e6f115664
Bruno Banho <bbanho@users.noreply.github.com>
2026-08-11 22:13:11 -0300

    docs: agendamento universal — design inicial (visão, diagrama, UC mínimos)

--- ampla/assets/chunks/swimlanesDiagram-ULZ7WXOC.BocARdOU.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN.DW82VMb2.js";import{_ as a}from"../app.C_At6Bc0.js";import"./chunk-5VM5RSS4.DXwmFAfF.js";import"./chunk-XXDRQBXY.B1znXCEI.js";import"./chunk-KBJHAD2P.D1nD2uff.js";import"./chunk-2GRJ4B5K.puqcB9Pi.js";import"./channel.2qL2_SUw.js";import"./framework.D7Ov6LBE.js";import"./theme.El1VyjI4.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
---
+++ ampla/assets/chunks/swimlanesDiagram-ULZ7WXOC.D1gmfh69.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.DQPrimnt.js";import{_ as a}from"../app.B7FPCsf-.js";import"./chunk-5VM5RSS4.B4K8iaiL.js";import"./chunk-XXDRQBXY.dazdM6v0.js";import"./chunk-KBJHAD2P.HcveTiYu.js";import"./chunk-2GRJ4B5K.qkr-GSia.js";import"./channel.d7s6rlOd.js";import"./framework.D7Ov6LBE.js";import"./theme.pHk1Q93P.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
