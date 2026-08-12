fd02c3861ffd67e150c7a1903266e2fbdcd69161
Bruno Banho <bbanho@users.noreply.github.com>
2026-08-11 22:02:16 -0300

    docs: adiciona proposta Agendamento Universal (pivô da próxima sessão)

--- ampla/assets/chunks/swimlanesDiagram-ULZ7WXOC.CSz6PTSc.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN.DyOI9p5n.js";import{_ as a}from"../app.C96I4VUa.js";import"./chunk-5VM5RSS4.Cib_G-LT.js";import"./chunk-XXDRQBXY.BHuu7VUu.js";import"./chunk-KBJHAD2P.CHmAjnVv.js";import"./chunk-2GRJ4B5K.D5zIPWDP.js";import"./channel.jDws9Mwk.js";import"./framework.D7Ov6LBE.js";import"./theme.Dag0Kzzs.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
