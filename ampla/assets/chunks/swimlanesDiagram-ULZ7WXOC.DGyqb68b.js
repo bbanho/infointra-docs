1f27780531c946d2224bc541f6f6be46875d0a62
Bruno Banho <bbanho@users.noreply.github.com>
2026-08-11 23:30:09 -0300

    docs: crítica arquitetônica do Agendamento Universal + revisão do intraclinica-ng

---
+++ ampla/assets/chunks/swimlanesDiagram-ULZ7WXOC.DGyqb68b.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.Bn0aPztV.js";import{_ as a}from"../app.k3CPFHST.js";import"./chunk-5VM5RSS4.BqtwG1Vi.js";import"./chunk-XXDRQBXY.DvHahtKn.js";import"./chunk-KBJHAD2P.n0sFuWOB.js";import"./chunk-2GRJ4B5K.lqixQ0MA.js";import"./channel.COJnMrBA.js";import"./framework.D7Ov6LBE.js";import"./theme.CuMq80_q.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
--- ampla/assets/chunks/swimlanesDiagram-ULZ7WXOC.DOqJ6VGO.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN.Cgp_31oh.js";import{_ as a}from"../app.BHtOsYuJ.js";import"./chunk-5VM5RSS4.Hedm6udX.js";import"./chunk-XXDRQBXY.BQl4necI.js";import"./chunk-KBJHAD2P.CJrn4nYt.js";import"./chunk-2GRJ4B5K.6nK-mgDp.js";import"./channel.Bj5-dftV.js";import"./framework.D7Ov6LBE.js";import"./theme.DaRhVC4S.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
